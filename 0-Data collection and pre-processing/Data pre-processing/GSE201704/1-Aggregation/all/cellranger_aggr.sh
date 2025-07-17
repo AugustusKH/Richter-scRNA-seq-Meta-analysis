7e2f01138af20edecc6ee0b5db562650cd933751
Pakorn Sagulkoo <pakorn.sagulkoo@cmu.ac.th>
2025-07-17 23:28:26 +0100

    Create cellranger_aggr.sh

---
+++ 0-Data collection and pre-processing/Data pre-processing/GSE201704/1-Aggregation/all/cellranger_aggr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=cellranger_run     # Job name
+#SBATCH --output=output.txt           # Stdout log
+#SBATCH --error=error.txt             # Stderr log
+#SBATCH --time=15-00:00:00            # D-HH:MM:SS
+#SBATCH --ntasks=1                    # Number of tasks
+#SBATCH --cpus-per-task=8             # CPU cores per task
+#SBATCH --mem=64G                     # Memory per node
+#SBATCH --partition=low               # Partition name
+
+# Load cellranger
+#module load cellranger/6.1.2
+export PATH=$HOME/local/cellranger-9.0.1:$PATH
+
+# Base path to main directory
+cd /home/pakorns/fastq_files/scRNAseq/GSE201704/fastq/sample_count/data_aggr/all
+
+# Run cellranger multi
+cellranger aggr --id=all_aggr --csv=all_aggr.csv
