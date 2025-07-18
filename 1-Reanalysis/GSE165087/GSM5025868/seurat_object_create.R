b339b41de7dcb12137b3d22cf232fe3dbc35a500
Pakorn Sagulkoo <pakorn.sagulkoo@cmu.ac.th>
2025-07-18 22:28:19 +0100

    Update seurat_object_create.R

--- 1-Reanalysis/GSE165087/GSM5025868/seurat_object_create.R
+++ 1-Reanalysis/GSE165087/GSM5025868/seurat_object_create.R
@@ -14,7 +14,7 @@ GSM5025868.seu <- CreateSeuratObject(counts = GSM5025868.data, min.cells = 3, mi
 # Define other columns in meta-data
 GSM5025868.seu[['ref']] <- 'Penter et al. (2021)' 
 GSM5025868.seu[['tissue']] <- 'PB'
-GSM5025868.seu[['case']] <- 1
+GSM5025868.seu[['case']] <- 2
 GSM5025868.seu[['cond']] <- 'CLL'
 GSM5025868.seu[['sorted']] <- 'whole'
 GSM5025868.seu[['stage']] <- 'FCR_relapse_timepoint2'
