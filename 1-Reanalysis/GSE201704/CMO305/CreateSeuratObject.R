7831f9ea3c9944933d5e5a856c3a1c0c317bf428
Pakorn Sagulkoo <pakorn.sagulkoo@cmu.ac.th>
2025-07-21 13:18:06 +0100

    Create CreateSeuratObject.R

---
+++ 1-Reanalysis/GSE201704/CMO305/CreateSeuratObject.R
@@ -0,0 +1,23 @@
+# Import the relevant packages
+library(dplyr)
+library(Seurat)
+
+# Find the working path
+getwd()
+
+# Read 10x Genomics data
+CMO305.data <- Read10X(data.dir = '/home/pakorns/hp-storage/scRNAseq/GSE201704/GSM6069857/CMO305/count_matrices/count/filtered_feature_bc_matrix')
+
+# Create Seurat object
+CMO305.seu <- CreateSeuratObject(counts = CMO305.data[['Gene Expression']], min.features = 500, min.cells = 1)
+
+# Define other columns in meta-data
+CMO305.seu[['HTO']] <- 'CMO305'
+CMO305.seu[['ref']] <- 'Rejeski et al. (2022)'
+CMO305.seu[['tissue']] <- 'PB'
+CMO305.seu[['case']] <- 1
+CMO305.seu[['cond']] <- 'RT'
+CMO305.seu[['sorted']] <- 'whole'
+
+# Save the object as .rds file
+SaveSeuratRds(CMO305.seu, file='/mnt/hp-storage/users/pakorns/scRNAseq/GSE201704/GSM6069857/CMO305/CMO305.rds')
