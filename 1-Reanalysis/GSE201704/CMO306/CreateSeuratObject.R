a617e48fc7df62e1e5720d726503f9817171f516
Pakorn Sagulkoo <pakorn.sagulkoo@cmu.ac.th>
2025-07-21 13:22:45 +0100

    Update CreateSeuratObject.R

--- 1-Reanalysis/GSE201704/CMO306/CreateSeuratObject.R
+++ 1-Reanalysis/GSE201704/CMO306/CreateSeuratObject.R
@@ -11,6 +11,10 @@ CMO306.data <- Read10X(data.dir = '/home/pakorns/hp-storage/scRNAseq/GSE201704/G
 # Create Seurat object
 CMO306.seu <- CreateSeuratObject(counts = CMO306.data[['Gene Expression']], min.features = 500, min.cells = 1)
 
+# QC
+CMO306.seu[["percent.mt"]] <- PercentageFeatureSet(CMO306.seu, pattern = "^MT-")
+CMO306.seu <- subset(CMO306.seu, subset = percent.mt < 20)
+
 # Define other columns in meta-data
 CMO306.seu[['HTO']] <- 'CMO306'
 CMO306.seu[['ref']] <- 'Rejeski et al. (2022)'
